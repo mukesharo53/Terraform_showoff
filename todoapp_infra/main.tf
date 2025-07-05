7bc2a6438103fb1a2d7d052b569f217eee664e56
mukesharo53@gmail.com <mukesharo53@gmail.com>
2025-07-05 16:42:37 +0530

    added 1 rg group

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -10,6 +10,12 @@ module "resource_group1" {
   resource_group_location = "canada central"
 }
 
+module "resource_group2" {
+  source = "../modules/azurerm_resource_group"
+  resource_group_name = "101-rg-us"
+  resource_group_location = "canada central"
+}
+
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"
