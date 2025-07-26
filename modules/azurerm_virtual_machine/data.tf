485b9b41711b87c861e04bc60c86615b67188d23
mukesharo53 <mukesharo53@gmail.com>
2025-07-26 10:24:42 +0530

    Update data.tf

--- modules/azurerm_virtual_machine/data.tf
+++ modules/azurerm_virtual_machine/data.tf
@@ -15,7 +15,7 @@ data "azurerm_key_vault" "kv" {
 }
 
 data "azurerm_key_vault_secret" "vm-username" {
-  name         = ""
+  name         = var.vm_username_secret_name
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
